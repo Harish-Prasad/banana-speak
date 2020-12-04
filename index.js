fd0e5a09a76aa9dc34df1ebe5aa6b9d4dd477a25
Harish-Prasad <hp.harishprasad@gmail.com>
2020-12-04 18:33:00 +0530

    implemented translate functionality

--- index.js
+++ index.js
@@ -2,7 +2,37 @@
 
 var textInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
+var output =  document.querySelector("#output");
 
 
 console.log(textInput);
 console.log(btnTranslate);
+console.log(output)
+
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedUrl(txtMessage){
+    return url+"?"+"text="+text;
+}
+
+
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server, Try again after some time... ")
+}
+
+btnTranslate.addEventListener("click","clickHandler");
+
+function clickHandler() {
+    var inputText = txtInput.value; 
+
+    
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; 
+           })
+        .catch(errorHandler)
+};
